0bf873729b5820248464169c5b2488cb0043be7e
memexpo <dmitry@marscolony.io>
2023-07-21 23:17:40 +0200

    filter by safe

--- src/listeners/Listener.ts
+++ src/listeners/Listener.ts
@@ -4,11 +4,17 @@ const TOKEN_ABI = [
   "event Transfer(address indexed src, address indexed dst, uint val)",
 ];
 
-export const listen = async (chain: string, rpc: string, constract: `0x${string}`) => {
+export const listen = async (
+  chain: string,
+  rpc: string,
+  constract: `0x${string}`,
+  safe: `0x${string}`,
+) => {
   const provider = new ethers.providers.WebSocketProvider(rpc);
   const contract = new ethers.Contract(constract, TOKEN_ABI, provider);
 
   contract.on("Transfer", (src, dst, val) => {
+    if (dst !== safe) return;
     console.log(chain, src, dst, val.toString());
   });
 };
