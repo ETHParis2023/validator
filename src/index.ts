15c292285361a70e300c419ad9e4f469586cbe16
memexpo <dmitry@marscolony.io>
2023-07-21 23:18:19 +0200

    filter by safe

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { GNOSIS_NODE, GNOSIS_USDC, POLYGON_NODE, POLYGON_USDC } from "./constants";
+import { GNOSIS_NODE, GNOSIS_SAFE, GNOSIS_USDC, POLYGON_NODE, POLYGON_SAFE, POLYGON_USDC } from "./constants";
 import { listen } from "./listeners/Listener";
 
 const main = async () => {
   await Promise.all([
-    listen('Polygon', POLYGON_NODE, POLYGON_USDC),
-    listen('Gnosis', GNOSIS_NODE, GNOSIS_USDC),
+    listen('Polygon', POLYGON_NODE, POLYGON_USDC, POLYGON_SAFE),
+    listen('Gnosis', GNOSIS_NODE, GNOSIS_USDC, GNOSIS_SAFE),
   ]);
 };
 
